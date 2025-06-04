1c634025890e85aaaa0a59200f3b884ccf2a8b3e
huangjunyao123 <148626538+Yao12345312@users.noreply.github.com>
2025-06-04 22:30:09 +0800

    Update CRAIC-offboard-test.cpp

--- src/CRAIC-offboard-test.cpp
+++ src/CRAIC-offboard-test.cpp
@@ -1,7 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
-#include "mavros_msgs/PositionTarget.h"
+#include "mavros_msgs/msg/position_target.hpp"
 #include "mavros_msgs/msg/state.hpp"
 #include "mavros_msgs/srv/set_mode.hpp"
 #include "mavros_msgs/srv/command_bool.hpp"
